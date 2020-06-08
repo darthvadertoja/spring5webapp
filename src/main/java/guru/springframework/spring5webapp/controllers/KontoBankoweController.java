4f6adc1b370eef902191c9432bb075645e686d20
darthvadertoja <mkarolczak@gmail.com>
2020-06-08 20:09:02 +0200

    my 4th commit - post method for konto bankowe

--- src/main/java/guru/springframework/spring5webapp/controllers/KontoBankoweController.java
+++ src/main/java/guru/springframework/spring5webapp/controllers/KontoBankoweController.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
@@ -16,6 +18,34 @@ public class KontoBankoweController
     @Autowired
     private KontoBankoweService kontoService;
 
+
+    @RequestMapping("/pokazKonta")
+    public String pokazKonta(Model model)
+    {
+        model.addAttribute("listaKont", kontoService.listAll());
+        return "listaKontBankowych";
+    }
+
+    @RequestMapping("/noweko")
+    public String utworzNoweKonto( Model model)
+    {
+        KontoBankowe noweKonto = new KontoBankowe();
+        model.addAttribute("konto", noweKonto);
+        return "noweKonto";
+    }
+
+    //metoda POST - przez formularz przekazuje parametry
+    @RequestMapping(value = "/zapiszKonto", method = RequestMethod.POST)
+    public String saveKontoBankowe(@ModelAttribute("konto") KontoBankowe kontoCoDaliMiNaNecie)
+    {
+        kontoService.save(kontoCoDaliMiNaNecie);
+        return "kontoTemplate";
+    }
+
+
+
+
+    //metoda GET - przez pasek adresu w przegladarce przekazuje parametry
     ///  http://localhost:8899/save?i=michal&s=12&n=hdh373
     @RequestMapping("/save")
     public String saveKontoBankowe(@RequestParam("i") String imieWlasciciela,
--- src/main/java/guru/springframework/spring5webapp/services/KontoBankoweService.java
+++ src/main/java/guru/springframework/spring5webapp/services/KontoBankoweService.java
@@ -3,7 +3,10 @@ package guru.springframework.spring5webapp.services;
 import guru.springframework.spring5webapp.domain.KontoBankowe;
 import guru.springframework.spring5webapp.repositories.KontoBankoweRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class KontoBankoweService
@@ -15,4 +18,11 @@ public class KontoBankoweService
     {
         kontoBankoweRepository.save(kontoBankowe);
     }
+
+    public List<KontoBankowe> listAll()
+    {
+
+        return (List<KontoBankowe>) kontoBankoweRepository.findAll();
+    }
+
 }
